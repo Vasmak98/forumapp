b0369d56583bb3a7f98c0fffa2d9e3cdb6d7386f
vasmak98 <vasilisandreou1198@gmail.com>
2023-05-10 12:30:49 +0300

    second comit

--- my_first_app/resources/views/header.blade.php
+++ my_first_app/resources/views/header.blade.php
@@ -23,7 +23,9 @@
           <li class="nav-item">
               <a class="nav-link" href="#">Τράπεζα</a>
           </li>
-          <li class="nav-item ml-auto">
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
             <a class="nav-link" href="#">Log out</a>
         </li>        
       </ul>
