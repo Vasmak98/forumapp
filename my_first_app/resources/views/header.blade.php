18ecb1126ece79f81eccfebe96e01b5963cd4c92
vasmak98 <vasilisandreou1198@gmail.com>
2023-05-11 10:23:16 +0300

    second comit

--- my_first_app/resources/views/header.blade.php
+++ my_first_app/resources/views/header.blade.php
@@ -1,15 +1,23 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-  <a class="navbar-brand" href="#">Φόρουμ</a>
+  {{-- <a class="navbar-brand" href="#">Φόρουμ</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-  </button>
+  </button> --}}
   <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
           <li class="nav-item active">
               <a class="nav-link" href="#">Home</a>
           </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Ανακοινώσεις</a>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Ανακοινώσεις
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Ανακοινώσεις Τάξεων/Τμημάτων</a>
+              <a class="dropdown-item" href="#">Ανακοινώσεις για συγκεκριμένο Μάθημα</a>
+              <a class="dropdown-item" href="#">Ειδικές Ανακοινώσεις</a>
+              <a class="dropdown-item" href="#">Ανακοινώσεις Διαγωνισμάτων</a>
+            </div>
           </li>
           <li class="nav-item">
               <a class="nav-link" href="#">Βαθμοί Διαγωνισμάτων</a>
@@ -17,8 +25,14 @@
           <li class="nav-item">
               <a class="nav-link" href="#">Απουσίες</a>
           </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Μαθητές</a>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownStudents" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Μαθητές
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownStudents">
+              <a class="dropdown-item" href="#">Αξιολόγηση Μαθητών</a>
+              <a class="dropdown-item" href="#">Προφίλ Μαθητών</a>
+            </div>
           </li>
           <li class="nav-item">
               <a class="nav-link" href="#">Τράπεζα</a>
--- my_first_app/resources/views/master.blade.php
+++ my_first_app/resources/views/master.blade.php
@@ -9,10 +9,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 
     <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
     <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.3/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
 
 </head>
 <body>
