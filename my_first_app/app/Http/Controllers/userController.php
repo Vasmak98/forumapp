e26803e138b521618017d01cba631d823b890bf9
vasmak98 <vasilisandreou1198@gmail.com>
2023-05-11 14:52:47 +0300

    second comit

--- my_first_app/app/Http/Controllers/userController.php
+++ my_first_app/app/Http/Controllers/userController.php
@@ -1,16 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class userController extends Controller
 {
     //
     function login(Request $req)
     {
-        return $req->input();
-        
+        $user = User::where(['email' => $req->email])->first();
+        if(!$user || !Hash::check($req->password,$user->password))
+        {
+            return "Username or password is not match";
+        }
+        else
+        {
+            return "redirect('/');";
+        }
+
     }
 }
--- my_first_app/resources/views/login.blade.php
+++ my_first_app/resources/views/login.blade.php
@@ -4,14 +4,15 @@
     <div class="row justify-content-center">
       <div class="col-md-6">
         <form action="/login" method="POST">
+          @csrf
           <div class="form-group">
             <label for="emailInput" class="font-weight-bold">Email</label>
-            <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" name="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div class="form-group">
             <label for="passwordInput" class="font-weight-bold">Password</label>
-            <input type="password" class="form-control" id="passwordInput" placeholder="Password">
+            <input type="password" name="password" class="form-control" id="passwordInput" placeholder="Password">
           </div>
           <button type="submit" class="btn btn-primary mt-3">Login</button>
         </form>
