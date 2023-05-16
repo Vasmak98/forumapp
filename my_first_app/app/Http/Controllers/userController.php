9045144cef83b0998669ab1ae05d0926fd780e67
vasmak98 <vasilisandreou1198@gmail.com>
2023-05-16 12:21:32 +0300

    login

--- my_first_app/app/Http/Controllers/userController.php
+++ my_first_app/app/Http/Controllers/userController.php
@@ -13,13 +13,14 @@ class userController extends Controller
     function login(Request $req)
     {
         $user = User::where(['email' => $req->email])->first();
-        if(!$user || !Hash::check($req->password,$user->password))
+        if(!$user || Hash::check($req->password,$user->password))
         {
             return "Username or password is not match";
         }
         else
         {
-            return "redirect('/');";
+            $req->session()->put('user', $user);
+            return redirect('/');
         }
 
     }
